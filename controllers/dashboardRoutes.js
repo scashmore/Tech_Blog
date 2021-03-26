4d5f9019dd5e725390463960d678debe88c04d13
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-25 23:41:51 -0400

    daah routs

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -67,7 +67,7 @@ router.get('/', withAuth, (req, res) => {
     })
       .then(dbPostData => {
         if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
+          res.status(404).json({ message: 'Post not found' });
           return;
         }
   
