15ca6ba51566c4e368f7af3a57fc5c34cbeab584
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-25 21:54:28 -0400

    index rout

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,16 @@
+  
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
