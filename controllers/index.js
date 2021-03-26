d8de3db580a19a77ef52de2c79046c1e3f783d28
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-25 22:12:08 -0400

    model associations

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
